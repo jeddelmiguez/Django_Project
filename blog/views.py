6f81486010e3b75f5806128f225e714ae5dd50e9
Jed Delmiguez <jeddelmiguez@Jeds-MBP.hub>
2020-01-27 22:56:04 +1000

    Part 03 - Templates - HTML Files.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Dummy data to be used in displaying into the home.html file with a database yet
+# See 'context' under def home (request): section below
+# See home.html <body> code for the for loop to display this data
+posts = [
+    {
+        'author': 'JedRD',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 27, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 28, 2020'
+    }    
+]
 
 # Create your views here.
 # Functions
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts #Passing the data into the home.html file
+    }
+    return render(request, 'blog/home.html', context) # 'context' added as third argument
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
