a71176c1bcff693461ffd27139d00f904fa052ba
Jed Delmiguez <jeddelmiguez@Jeds-MBP.hub>
2020-01-28 00:07:11 +1000

    Part 05 - Database and Migrations.

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
-
-# Dummy data to be used in displaying into the home.html file with a database yet
-# See 'context' under def home (request): section below
-# See home.html <body> code for the for loop to display this data
-posts = [
-    {
-        'author': 'JedRD',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 27, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 28, 2020'
-    }    
-]
-
-# Create your views here.
-# Functions
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts #Passing the data into the home.html file
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', context) # 'context' added as third argument
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
